271bcb02988592fcb52d64ec39373d0f5f847ebe
Martin Hajný <hajnina11@gmail.com>
2018-01-23 13:28:13 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+  </head>
+  <body>
+    <h1>Hello world.</h2>
+      <?php echo "<h2>Yay. It's PHP</h2>"; ?>
+  </body>
+</html>
